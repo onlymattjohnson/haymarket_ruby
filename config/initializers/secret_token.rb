7e5b74a7d3b263cdba9a949b846b3d40e7bcc0cb
Matt Johnson <mjohn23@gmail.com>
2013-08-19 22:20:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HaymarketRuby::Application.config.secret_key_base = '82fb51adb1ebb34e1c0f7f5bc9adeb7e0cab6bf4f235cf714f72f081f9e7e4beaf6f0025f72a5490807892e245aa593f8b429d13e6377d8d9254ab089d533dae'
